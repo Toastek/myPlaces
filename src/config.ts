51e35d968c8ec892dcf405448daf161d3d63791e
Toastek <giacomo.kersevan@epitech.eu>
2019-10-21 16:53:51 +0200

    added config.ts for firebase usage

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+	fire: {
+		apiKey: "AIzaSyDPT7iW5MEsmrib98dSAhE2AYhj4tIKi4g",
+    authDomain: "myplaces-70bb2.firebaseapp.com",
+    databaseURL: "https://myplaces-70bb2.firebaseio.com",
+    projectId: "myplaces-70bb2",
+    storageBucket: "myplaces-70bb2.appspot.com",
+    messagingSenderId: "747745640732",
+    appId: "1:747745640732:web:c3aeac943259bd757bbad9"
+	}
+};
